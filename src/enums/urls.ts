bbcaca7c4d90b559b32ad270dd6e88e52ac4c50a
gerssivaldosantos <canalandows@gmail.com>
2022-08-15 18:59:27 -0300

    feat: add url enums

---
+++ src/enums/urls.ts
@@ -0,0 +1,6 @@
+export enum UrlEnum {
+    BASE_URL = 'http://localhost:3000/',
+    BACKEND_VAGAS_GITHUB_URL = 'https://github.com/backend-br/vagas/issues',
+    GITHUB_QUERY_URL_BASE = '&q=is%3Aissue+is%3Aopen',
+    GITHUB = 'https://github.com'
+}
