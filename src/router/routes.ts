e3e38aa7c9404074c2df1b1ec86187257e4f5453
gerssivaldosantos <canalandows@gmail.com>
2022-08-19 22:56:35 -0300

    refactor: remove catch all and add HomePage route

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -4,15 +4,13 @@ const routes: RouteRecordRaw[] = [
   {
     path: '/',
     component: () => import('layouts/MainLayout.vue'),
-    children: [{ path: '', component: () => import('pages/IndexPage.vue') }]
-  },
+    children: [{ path: '/home', component: () => import('pages/HomePage.vue') }]
+  }
 
-  // Always leave this as last one,
-  // but you can also remove it
-  {
+  /* {
     path: '/:catchAll(.*)*',
     component: () => import('pages/ErrorNotFound.vue')
-  }
+  } */
 ]
 
 export default routes
