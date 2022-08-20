7b1fd380a5507737617a2067f93e52d2eb8b95c7
gerssivaldosantos <canalandows@gmail.com>
2022-08-20 10:15:29 -0300

    refactor: remove main route children

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -4,7 +4,7 @@ const routes: RouteRecordRaw[] = [
   {
     path: '/',
     component: () => import('layouts/MainLayout.vue'),
-    children: [{ path: '/home', component: () => import('pages/HomePage.vue') }]
+    children: [{ path: '', component: () => import('pages/HomePage.vue') }]
   }
 
   /* {
