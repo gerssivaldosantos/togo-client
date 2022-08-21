f4e8ca4c5f4c8e52e45cd1aae6371d74704f00d0
gerssivaldosantos <canalandows@gmail.com>
2022-08-21 08:55:50 -0300

    refactor: change literal html template to new version using v-for and v-bind

--- src/components/shareBottomDialog.vue
+++ src/components/shareBottomDialog.vue
@@ -1,35 +1,11 @@
 <template>
   <q-dialog position="bottom" ref="dialogRef" @hide="onDialogHide">
     <q-card class="q-dialog-plugin" style="width: 350px">
-      <q-card-actions align="center">
+      <q-card-actions style="padding: 20px" align="center">
         <q-btn
-          :href="`https://www.linkedin.com/shareArticle?mini=true&url=${props.url}`"
-          target="_blank"
-          flat
-          round
-          icon="fa-brands fa-linkedin"
-        />
-        <q-btn
-        :href="`https://telegram.me/share/url?url=${props.url}`"
-        target="_blank"
-        flat
-        round
-        icon="fa-brands fa-telegram"
-        />
-        <q-btn
-          :href="`whatsapp://send?text=${props.url}`"
-          target="_blank"
-          data-action="share/whatsapp/share"
-          flat
-          round
-          icon="fa-brands fa-whatsapp"
-        />
-        <q-btn
-          :href="`https://www.facebook.com/sharer/sharer.php?u=${props.url}`"
-          target="_blank"
-          flat
-          round
-          icon="fa-brands fa-facebook"
+          v-for="option in buttonsOptions"
+          v-bind:key="option.href"
+          v-bind="option"
         />
       </q-card-actions>
     </q-card>
@@ -38,17 +14,57 @@
 
 <script setup lang="ts">
 import { ref } from 'vue'
-
-const dialogRef = ref()
-
-const onDialogHide = () => {
-  dialogRef.value.hide()
-}
-
 const props = defineProps({
   url: {
     type: String,
     default: ''
   }
 })
+
+const dialogRef = ref()
+
+const buttonsOptions = ref([
+  {
+    href: `https://www.linkedin.com/shareArticle?mini:true&url:${props.url}`,
+    target: '_blank',
+    flat: true,
+    round: true,
+    icon: 'fa-brands fa-linkedin',
+    color: 'secondary',
+    size: 'medium'
+  },
+  {
+    href: `https://telegram.me/share/url?url=${props.url}`,
+    icon: 'fa-brands fa-telegram',
+    target: '_blank',
+    flat: true,
+    round: true,
+    color: 'secondary',
+    size: 'medium'
+  },
+  {
+    href: `whatsapp://send?text=${props.url}`,
+    icon: 'fa-brands fa-whatsapp',
+    target: '_blank',
+    flat: true,
+    round: true,
+    color: 'secondary',
+    size: 'medium'
+  },
+  {
+    href: `https://www.facebook.com/sharer/sharer.php?u=${props.url}`,
+    icon: 'fa-brands fa-facebook',
+    target: '_blank',
+    flat: true,
+    round: true,
+    color: 'secondary',
+    size: 'medium'
+  }
+])
+
+const onDialogHide = () => {
+  dialogRef.value.hide()
+}
 </script>
+
+<style lang="scss" scoped></style>
